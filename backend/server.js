23fede4b82f8b1e03ff565331cdc415037c0c6b6
Patrik <pgalina@foi.hr>
2021-06-25 13:19:57 +0200

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const userRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
@@ -28,6 +29,7 @@ mongoose.connect(
 );
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
