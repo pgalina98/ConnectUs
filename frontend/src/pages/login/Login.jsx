316dd7be3d86572bebdecf7678facf67c99a4903
Patrik <pgalina@foi.hr>
2021-06-28 10:52:10 +0200

    Added validation for Login page [react-hook-form]

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -10,27 +10,39 @@ import {
   InputAdornment,
   IconButton,
 } from "@material-ui/core";
-import { Visibility, VisibilityOff } from "@material-ui/icons";
+import {
+  Visibility,
+  VisibilityOff,
+  CheckCircleOutlineOutlined,
+  ErrorOutlineOutlined,
+} from "@material-ui/icons";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [password, setPassword] = useState({
-    value: "",
-    showPassword: false,
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    mode: "onChange",
   });
 
   const handleClickShowPassword = () => {
-    setPassword({ ...password, showPassword: !password.showPassword });
+    setShowPassword(!showPassword);
   };
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
-  const handleLoginSubmit = () => {
+  const handleLoginSubmit = (data) => {
     //TODO -> Check user data - send Axios request to API
-    console.log("Username submit:" + username);
+    console.log("Submited data e-mail: " + data.email);
+    console.log("Submited data password: " + data.password);
   };
 
   return (
@@ -43,21 +55,58 @@ export default function Login() {
           </span>
         </div>
         <div className="loginWrapperRight">
-          <div className="loginWrapperRightContainer">
+          <form
+            className="loginWrapperRightContainer"
+            onSubmit={handleSubmit(handleLoginSubmit)}
+          >
             <TextField
               id="email"
               label="Enter your e-mail"
               variant="outlined"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
+              {...register("email", {
+                required: { value: true, message: "Email is required" },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "E-mail address format is invalid.",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "E-mail address can contains max 50 characters.",
+                },
+              })}
             />
+            {errors.email ? (
+              <ErrorMessage
+                name="email"
+                errors={errors}
+                render={({ message }) => (
+                  <div className="loginWrapperRightContainerErrorMessage">
+                    <ErrorOutlineOutlined />
+                    <p>{message}</p>
+                  </div>
+                )}
+              />
+            ) : (
+              getValues("email") && (
+                <div className="loginWrapperRightContainerCorrectMessage">
+                  <CheckCircleOutlineOutlined />
+                  <p>Correct.</p>
+                </div>
+              )
+            )}
+
             <FormControl variant="outlined">
               <InputLabel htmlFor="password">Enter your password</InputLabel>
               <OutlinedInput
                 id="password"
-                type={password.showPassword ? "text" : "password"}
-                value={password.value}
-                onChange={(event) => setPassword({ value: event.target.value })}
+                type={showPassword ? "text" : "password"}
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: {
+                    value: 6,
+                    message: "Password must contains at least 6 characters.",
+                  },
+                })}
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
@@ -66,35 +115,55 @@ export default function Login() {
                       onMouseDown={handleMouseDownPassword}
                       edge="end"
                     >
-                      {password.showPassword ? (
-                        <Visibility />
-                      ) : (
-                        <VisibilityOff />
-                      )}
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
                 }
                 labelWidth={150}
               />
+              {errors.password ? (
+                <ErrorMessage
+                  name="password"
+                  errors={errors}
+                  render={({ message }) => (
+                    <div
+                      className="loginWrapperRightContainerErrorMessage 
+                                 loginWrapperRightContainerAddMarginToMessage"
+                    >
+                      <ErrorOutlineOutlined />
+                      <p>{message}</p>
+                    </div>
+                  )}
+                />
+              ) : (
+                getValues("password") && (
+                  <div
+                    className="loginWrapperRightContainerCorrectMessage 
+                               loginWrapperRightContainerAddMarginToMessage"
+                  >
+                    <CheckCircleOutlineOutlined />
+                    <p>Correct.</p>
+                  </div>
+                )
+              )}
             </FormControl>
             <FormControlLabel
               className="loginWrapperRightContainerRememberPassword"
               control={<Checkbox name="checkedB" color="primary" />}
               label="Remember password"
             />
-            <button
+            <input
+              type="submit"
+              value="Log In"
               className="loginWrapperRightContainerLoginButton"
-              onClick={handleLoginSubmit}
-            >
-              Log In
-            </button>
+            />
             <span className="loginWrapperRightContainerForgotPassword">
               Forgot Password?
             </span>
             <button className="loginWrapperRightContainerRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
